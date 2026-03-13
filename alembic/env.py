0cf90755f952dcf61a4c7584714cf1bd5c4c5429
hwanghyeonseo <hhseo1202@naver.com>
2026-03-13 12:57:48 +0900

    fix

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from app.db.base import Base
+import app.models
 target_metadata = Base.metadata
 
 # this is the Alembic Config object, which provides
@@ -17,11 +18,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
