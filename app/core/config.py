cb9606a558912d8dfc98b1a5e67cdd2d5dd82bf6
hwanghyeonseo <hhseo1202@naver.com>
2026-03-18 21:43:25 +0900

    config

--- app/core/config.py
+++ app/core/config.py
@@ -1,23 +1,20 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-#BaseSetting은 환경변수(env)와 설정값을 관리하는 클래스
+
 class Settings(BaseSettings):
     app_name: str = "News Monitoring API"
     api_v1_prefix: str = "/api/v1"
-    debug: bool = True #개발 모드 - 로그 많이 출력, 오류메시지 자세히 표시
-    
-    database_url: str = "postgresql+asyncpg://postgres:postgres@localhost:5432/news_monitoring"
-    
-    #JWT 설정들
-    secret_key: str = "-"
-    algorithm: str = "HS256" #JWT 암호화 방식(가장 많이 쓰이는 방식으로 일단)
+    debug: bool = True
+
+    database_url: str
+    secret_key: str
+    algorithm: str = "HS256"
     access_token_expire_minutes: int = 60
-    
-    #환경변수 설정
+
     model_config = SettingsConfigDict(
         env_file=".env",
         extra="ignore",
     )
 
-#설정 객체 생성 - 프로젝트 어디서든 불러올 수 있게
+
 settings = Settings()
